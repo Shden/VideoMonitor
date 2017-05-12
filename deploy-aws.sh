cc2e71d8a29757f685b7560ed40842ed0f2a9e22
Den Afanassiev <den@dataart.com>
2017-05-12 20:03:53 +0100

    deploy-aws.sh working version.

---
+++ deploy-aws.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Deploying HEAD version from github to AWS instance..."
+
+ssh -i ~/AWS/DA-AWS-Key.pem.txt ubuntu@ec2-35-160-123-29.us-west-2.compute.amazonaws.com "cd VideoMonitor && git pull"
+#"cd VideoMonitor && git pull"
